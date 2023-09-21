741c4c856d834b3fdc9335dd7caf01bdc8c2b9b4
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-21 21:55:01 +0530

    commit :: final push

--- build/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import spaceXController from "../controllers/index";
-import { validate } from "../middlewares/authorize";
-const router = express.Router();
-router.route("/data").get(validate, spaceXController.spaceXData);
-export default router;
